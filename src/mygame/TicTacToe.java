07c22505d73462b1386bd39234726347146b8fb9
RABI <thaparabi8848@gmail.com>
2023-06-14 17:05:11 +0545

    Improved version

--- src/mygame/TicTacToe.java
+++ src/mygame/TicTacToe.java
@@ -2,6 +2,7 @@ package mygame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -11,99 +12,220 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class TicTacToe {
+public class TicTacToe implements ActionListener{
 	boolean player1_turn;
 	boolean player2_turn;
+	String X= "X";
+	String O= "O";
 	
 	JButton[] buttons= new JButton[9];
 	
-	TicTacToe()
-	{
-		JFrame f= new JFrame("Tic Tac Toe");
-		f.setSize(600, 600);
-		BorderLayout b= new BorderLayout();
-		f.setLayout(b);
-		
-		JPanel p= new JPanel();
-		Dimension d= new Dimension(400, 400);
-		p.setPreferredSize(d);
-		p.setBackground(Color.black);
-		GridLayout g= new GridLayout(3,3,5,5);
-		p.setLayout(g);
-		
-		
-		String X= "X";
-		String O= "O";
-		
-		turn();
-		
-		int i;
-		for( i=0; i< buttons.length; i++)
-		{
-			buttons[i]= new JButton("");
-			Font font= new Font("Times New Roman", Font.BOLD, 30);
-			buttons[i].setFont(font);
-			p.add(buttons[i]);
-			final int setButton= i;
-			ActionListener al= new ActionListener()
-			{
-				@Override
-				public void actionPerformed(ActionEvent e) 
-				{
-				 JButton b = (JButton) e.getSource();
-                    if (b.getText().equals("") && player1_turn) 
-                    {
-                        buttons[setButton].setText(X); 
-                        player1_turn = false;
-                        player2_turn = true;
-                    }
-                    else if(b.getText().equals("") && player2_turn)
-                    {
-                    	 buttons[setButton].setText(O); 
-                    	 player1_turn = true;
-                         player2_turn = false;
-                    }
-				}
-			};
-			buttons[i].addActionListener(al);
-		}
-		
-		
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.add(p);
-		f.setVisible(true);
-		
-	}
-	
-	public void check()
-	{
-		if (buttons[0].getText().equals("X") &&
-		    buttons[1].getText().equals("X") &&
-		    buttons[2].getText().equals("X"))
-		{
-		    System.out.println("X wins");
-		}
-		else
-		{
-			System.out.println("O wins");
-		}
-	}
+	JFrame frame;
+	Font font;
+	JPanel p1;
+	JPanel p2;
+	JButton b;
+	JLabel l;
 	
 	void turn()
 	{
 		Random rand= new Random();
 		int turn= rand.nextInt(2);
-		player1_turn= (turn==0);
-		player2_turn= !player1_turn;
+		if(turn==0)
+		{
+			player1_turn= true;
+			player2_turn= false;
+		}
+		else
+		{
+			player1_turn= false;
+			player2_turn= true;
+		}
 	}
-	
-	
-	public static void main(String[] ar)
+	TicTacToe()
 	{
-		TicTacToe t= new TicTacToe();
-		t.check();
+		frame= new JFrame("TIC TAC TOE");
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GridLayout layout= new GridLayout(3,3, 10, 10);
+		p1= new JPanel();
+		p1.setBounds(80, 80, 400, 400);
+		p1.setBackground(Color.black);
+		p1.setLayout(layout);
+		
+		p2= new JPanel();
+		p2.setBounds(80, 10, 400, 60);
+		p2.setBackground(Color.black);
+		
+		
+		Container c= frame.getContentPane();
+		c.setLayout(null);
+		c.add(p1);
+		c.add(p2);
+		
+		font= new Font("Times New Roman", Font.BOLD, 40);
+		for(int i=0; i< buttons.length; i++)
+		{
+			buttons[i]= new JButton();
+			buttons[i].setFont(font);
+			buttons[i].setText("");
+			p1.add(buttons[i]);
+			buttons[i].addActionListener(this);
+		}
+		
+		l= new JLabel();
+		l.setForeground(new Color(100, 240, 230));
+		l.setBounds(0, 0, 100, 50);
+		l.setFont(font);
+		
+		font= new Font("Times New Roman", Font.BOLD, 20);
+		b= new JButton("Reset");
+		b.setBounds(230, 490, 100, 60);
+		b.setFocusable(false);
+		b.setFont(font);
+		b.addActionListener(this);
+		c.add(b);
+		p2.add(l);
+		
+		frame.add(p2);
+		frame.add(p1);
+		
+		font = new Font("Times New Roman", Font.BOLD, 40);
+		
+		turn();
 	}
+	
+	
+	
+	int i;
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		for(i=0; i< buttons.length; i++)
+		{
+			if(e.getSource() == b)
+			{
+				buttons[i].setEnabled(true);
+				buttons[i].setText("");
+			}
+			
+			if (e.getSource()== buttons[i] && player1_turn) 
+	        {
+	            buttons[i].setText(X); 
+	            player1_turn = false;
+	            player2_turn = true;
+	            l.setText("O turn");
+	            l.setFont(font);
+	        }
+			 else if(e.getSource()== buttons[i] && player2_turn)
+	        {
+	        	 buttons[i].setText(O); 
+	        	 player1_turn = true;
+	             player2_turn = false;
+	             l.setText("X turn");
+	             l.setFont(font);
+	        }
+		}
+		checkWinner();
+	}
+	
+	public void checkWinner() 
+	{
+		// x wins
+		if((buttons[0].getText()=="X") && (buttons[1].getText()=="X") && (buttons[2].getText()=="X")) 
+		{
+			xWins(0,1,2);
+		}
+		if((buttons[3].getText()=="X") && (buttons[4].getText()== "X")&& (buttons[5].getText()== "X"))
+		{
+			xWins(3,4,5);
+		}
+		if((buttons[6].getText()== "X") && (buttons[7].getText()== "X") && (buttons[8].getText()=="X"))
+		{
+			xWins(6,7,8);
+		}
+		
+		if((buttons[0].getText()== "X") && (buttons[3].getText()== "X") && (buttons[6].getText()=="X" ))
+		{
+			xWins(0,1,6);
+		}
+		if((buttons[1].getText()== "X") && (buttons[4].getText()== "X") && (buttons[7].getText()== "X"))
+		{
+			xWins(1,4,7);
+		}
+		if((buttons[2].getText()== "X") && (buttons[5].getText()== "X") && (buttons[8].getText()== "X"))
+		{
+			xWins(2,5,8);
+		}
+		if((buttons[0].getText()== "X") && (buttons[4].getText()=="X") && (buttons[8].getText()=="X"))
+		{
+			xWins(0,4,8);
+		}
+		if((buttons[2].getText()== "X") && (buttons[4].getText()=="X") && (buttons[6].getText()=="X"))
+		{
+			xWins(2,4,6);
+		}
+		
+		// o wins
+		if((buttons[0].getText()== "O") && (buttons[1].getText()=="O") && (buttons[2].getText()=="O"))
+		{
+			oWins(0,1,2);
+		}
+		if((buttons[3].getText()== "O") && (buttons[4].getText()=="O") && (buttons[5].getText()=="O"))
+		{
+			oWins(3,4,5);
+		}
+		if((buttons[6].getText()== "O") && (buttons[7].getText()=="O") && (buttons[8].getText()=="O"))
+		{
+			oWins(6,7,8);
+		}
+		if((buttons[0].getText()== "O") && (buttons[3].getText()=="O") && (buttons[6].getText()=="O"))
+		{
+			oWins(0,3,6);
+		}
+		if((buttons[1].getText()== "O") && (buttons[4].getText()=="O") && (buttons[7].getText()=="O"))
+		{
+			oWins(1,4,7);
+		}
+		if((buttons[2].getText()== "O") && (buttons[5].getText()=="O") && (buttons[8].getText()=="O"))
+		{
+			oWins(2,5,8);
+		}
+		if((buttons[0].getText()== "O") && (buttons[4].getText()=="O") && (buttons[8].getText()=="O"))
+		{
+			oWins(0,4,8);
+		}
+		if((buttons[2].getText()== "O") && (buttons[4].getText()=="O") && (buttons[6].getText()=="O"))
+		{
+			oWins(2,4,6);
+		}
+    }
 
+	public void xWins(int a,int b,int c) 
+	{
+		for(int i=0;i<9;i++) 
+		{
+			buttons[i].setEnabled(false);
+		}
+		JOptionPane.showMessageDialog(p1, "Player X wins");
+	}
+	
+	public void oWins(int a, int b, int c)
+	{
+		for(int i=0; i<9; i++)
+		{
+			buttons[i].setEnabled(false);
+		}
+		JOptionPane.showMessageDialog(p1, "Player O wins");
+	}
+	public static void main(String[] args)
+	{
+		new TicTacToe();
+	}
 }
